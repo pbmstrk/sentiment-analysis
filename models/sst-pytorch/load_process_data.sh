1b9abc36215684e0c1e726215e2a3f7ae5b54f09
pbmstrk <pbmstrk@users.noreply.github.com>
2020-09-28 21:56:37 +0100

    restructure

--- models/sst-pytorch/load_process_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir ./data/
-python3 download_data.py
--- sentiment-analysis/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='Sentiment-Analysis', version='1.0', packages=find_packages())
