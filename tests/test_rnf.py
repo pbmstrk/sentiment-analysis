fe97bda4fd13bda4748e64b8ec569cc4f0025a59
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2020-10-25 00:42:32 +0200

    Update to model tests (#10)

--- tests/test_rnf.py
+++ tests/test_rnf.py
@@ -1,160 +1,133 @@
 import torch
 import pytest
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, Dataset
 import pytorch_lightning as pl
 import numpy as np
 
 from text_classification.models import RNF
 
 
-def create_fake_data(low, high, dims):
+class FakeRNFDataset(Dataset):
 
-    return torch.randint(low, high, dims)
+    def __init__(self):
+        self.inputs = torch.randint(0, 10, (100, 15))
+        self.targets = torch.randint(0, 10, (100, ))
 
+    def __len__(self):
+        return len(self.targets)
 
-def create_test_dataloader_rnf(num_examples, batch_size, input_size, seq_len, num_class):
-    inputs = create_fake_data(0, input_size, (num_examples, seq_len))
-    targets = create_fake_data(0, num_class, (num_examples,))
-
-    dataset = TensorDataset(inputs, targets.long())
-    return DataLoader(dataset, batch_size=batch_size)
+    def __getitem__(self, idx):
+        return self.inputs[idx], self.targets[idx]
 
 
 class TestRNF:
 
     def test_output_shape(self):
-        input_size = 100
-        batch_size = 32
-        seq_len = 15
-        num_class = torch.randint(2, 10, size=(1,)).item()
-        inputs = create_fake_data(0, input_size, (batch_size, seq_len))
-        targets = None
 
-        batch = (inputs, targets)
+        data = FakeRNFDataset()
+        dataloader = DataLoader(data, batch_size=32)
 
-        model_args = {
-            'input_size': input_size,
-            'num_class': num_class
+        batch = next(iter(dataloader))
+
+        model_options = {
+            'input_size': 100,
+            'num_class': 10
         }
 
-        model = RNF(**model_args)
+        model = RNF(**model_options)
 
-        assert model(batch).shape == torch.Size([batch_size, num_class])
+        assert model(batch).shape == torch.Size([32, 10])
 
-    @staticmethod
-    def _parameters_are_eq(parameter_list_1, parameter_list_2):
-        results = []
-        for p1, p2 in zip(parameter_list_1, parameter_list_2):
-            results.append(torch.all(torch.eq(p1, p2)))
-        return all(results)
+
+    def run_model_test(self, trainer_options, model):
+
+        data = FakeRNFDataset()
+        dataloader = DataLoader(data, batch_size=32)
+
+        trainer = pl.Trainer(**trainer_options)
+
+        # check if model actually trains
+        # pytorch-lightning/blob/master/tests/base/develop_pipelines.py#L62-L69
+        initial_values = torch.tensor([torch.sum(torch.abs(x)) for x in model.parameters()])
+        result = trainer.fit(model, train_dataloader=dataloader, val_dataloaders=dataloader)
+        post_train_values = torch.tensor([torch.sum(torch.abs(x)) for x in model.parameters()])
+
+        assert result == 1
+
+        assert torch.norm(initial_values - post_train_values) > 0.1
+
+        test_result = trainer.test(model, dataloader)
 
     def test_forward_backward(self):
-
-        input_size = 100
-        num_class = torch.randint(2, 10, size=(1,)).item()
-
-        data_loader = create_test_dataloader_rnf(
-            num_examples=100, batch_size=32, input_size=100, seq_len=15, num_class=num_class
-        )
-        trainer = pl.Trainer(
-            progress_bar_refresh_rate=0,
-            max_steps=5,
-            num_sanity_val_steps=1,
-            checkpoint_callback=False,
-            logger=False,
-        )
-
-        model_args = {
-            'input_size': input_size,
-            'num_class': num_class
+        
+        trainer_options = {
+            "progress_bar_refresh_rate": 0,
+            "max_steps": 5,
+            "num_sanity_val_steps": 1,
+            "checkpoint_callback": False,
+            "logger": False,
         }
 
-        model_before = RNF(**model_args)
-        model_after = RNF(**model_args)
-        model_after.load_state_dict(model_before.state_dict())
+        model_options = {
+            'input_size': 100,
+            'num_class': 10
+        }
 
-        assert self._parameters_are_eq(
-            list(model_before.parameters()), list(model_after.parameters())
-        )
+        model = RNF(**model_options)
 
-        trainer.fit(model_after, data_loader, data_loader)
+        self.run_model_test(trainer_options, model)
 
-        assert not self._parameters_are_eq(
-            list(model_before.parameters()), list(model_after.parameters())
-        )
-
-        trainer.test(model_after, data_loader)
-
+        
     @pytest.mark.skipif(not torch.cuda.is_available(), reason="Test requires GPU")
     def test_forward_backward_gpu(self):
 
-        input_size = 100
-        num_class = torch.randint(2, 10, size=(1,)).item()
-
-        data_loader = create_test_dataloader_rnf(
-            num_examples=100, batch_size=32, input_size=100, seq_len=15, num_class=num_class
-        )
-        trainer = pl.Trainer(
-            gpus=1,
-            progress_bar_refresh_rate=0,
-            max_steps=5,
-            num_sanity_val_steps=1,
-            checkpoint_callback=False,
-            logger=False,
-        )
-
-        model_args = {
-            'input_size': input_size,
-            'num_class': num_class
+        trainer_options = {
+            "gpus": 1,
+            "progress_bar_refresh_rate": 0,
+            "max_steps": 5,
+            "num_sanity_val_steps": 1,
+            "checkpoint_callback": False,
+            "logger": False,
         }
 
-        model_before = RNF(**model_args)
-        model_after = RNF(**model_args)
-        model_after.load_state_dict(model_before.state_dict())
+        model_options = {
+            'input_size': 100,
+            'num_class': 10
+        }
 
-        assert self._parameters_are_eq(
-            list(model_before.parameters()), list(model_after.parameters())
-        )
+        model = RNF(**model_options)
 
-        trainer.fit(model_after, data_loader, data_loader)
-
-        assert not self._parameters_are_eq(
-            list(model_before.parameters()), list(model_after.parameters())
-        )
-
-        trainer.test(model_after, data_loader)
+        self.run_model_test(trainer_options, model)
 
     def test_weight_freeze(self):
 
-        input_size = 100
-        num_class = torch.randint(2, 10, size=(1,)).item()
+        data = FakeRNFDataset()
+        dataloader = DataLoader(data, batch_size=32)
+        
+        trainer_options = {
+            "progress_bar_refresh_rate": 0,
+            "max_steps": 5,
+            "num_sanity_val_steps": 0,
+            "checkpoint_callback": False,
+            "logger": False,
+        }
 
-        data_loader = create_test_dataloader_rnf(
-            num_examples=100, batch_size=32, input_size=100, seq_len=15, num_class=num_class
-        )
-        trainer = pl.Trainer(
-            progress_bar_refresh_rate=0,
-            max_steps=5,
-            num_sanity_val_steps=0,
-            checkpoint_callback=False,
-            logger=False,
-        )
+        trainer = pl.Trainer(**trainer_options)
 
-        embed_mat = np.random.randn(input_size, 300)
+        embed_mat = np.random.randn(100, 300)
 
-        model_args = {
-            'input_size': input_size,
-            'num_class': num_class,
+        model_options = {
+            'input_size': 100,
+            'num_class': 10,
             'freeze_embed': True,
             'embed_mat': embed_mat
         }
 
-        model_before = RNF(**model_args)
-        model_after = RNF(**model_args)
+        model_before = RNF(**model_options)
+        model_after = RNF(**model_options)
         model_after.load_state_dict(model_before.state_dict())
 
+        # check if embedding weight is correctly loaded and remains fixed
         assert torch.all(torch.eq(model_before.embedding.weight, model_after.embedding.weight))
-
-        trainer.fit(model_after, data_loader)
-
+        trainer.fit(model_after, dataloader)
         assert torch.all(torch.eq(model_before.embedding.weight, model_after.embedding.weight))
