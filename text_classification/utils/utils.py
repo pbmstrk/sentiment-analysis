e8bb873f0e6d3bdcbf3794d2a9661d5ae4ae202c
pbmstrk <pbmstrk@users.noreply.github.com>
2020-10-26 16:10:25 +0100

    Change in logic for downloading and extracting files
    Add boolean option to allow for only extracting files - behaviour before
    was not extract a file if it already exists

--- text_classification/utils/utils.py
+++ text_classification/utils/utils.py
@@ -1,7 +1,6 @@
 import gzip
 import os
 import shutil
-import zipfile
 from typing import Optional
 
 import requests
@@ -31,17 +30,16 @@ def download_file(
     # root and name are passed as parameters
     # filename is inferred from url
 
-    if os.path.exists(os.path.join(root, name)) and not override:
-        print(f"Path {root}/{name} already exists")
-        return
     # create directory if it doesn't exist
     if not os.path.exists(os.path.join(root, name)):
         os.makedirs(os.path.join(root, name))
 
     if not filename:
         _, filename = os.path.split(url)
+
     filepath = os.path.join(root, name, filename)
-    if not os.path.exists(filepath):
+
+    if not os.path.exists(filepath) or override:
         print(f"Downloading {filename} from {url}.\nSaving to:{filepath}")
 
         if not "drive.google.com" in url:
@@ -61,6 +59,9 @@ def download_file(
                 resp = session.get(url, stream=True)
 
             _process_resp(resp, filepath)
+    else:
+        print(f"Found file at {os.path.join(root, name)} stopping download.")
+        return 
 
     return filepath
 
@@ -89,8 +90,12 @@ def download_extract(
     root: str = ".data",
     override: bool = False,
     filename: Optional[str] = None,
+    extract_only: Optional[bool] = False
 ):
-
-    zfile = download_file(url, name, root=root, override=override, filename=filename)
     extract_dir = os.path.join(root, name)
-    extract_from_file(zfile, extract_dir)
+
+    if extract_only:
+        extract_from_file(os.path.join(root, name, filename), os.path.join(root, name))
+    else:
+        zfile = download_file(url, name, root=root, override=override, filename=filename)
+        extract_from_file(zfile, extract_dir)
