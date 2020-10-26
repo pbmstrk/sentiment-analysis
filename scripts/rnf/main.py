e63ad14221923721aa483ac94ff7a414fba50de3
pbmstrk <pbmstrk@users.noreply.github.com>
2020-10-26 13:46:56 +0100

    Minor bug fix in scripts [ci skip]
    Moved callback instance creation into function to fix behaviour in
    multirun setting

--- scripts/rnf/main.py
+++ scripts/rnf/main.py
@@ -1,67 +1,61 @@
+import logging
+
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.base import Callback
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
 from text_classification.datamodule import DataModule
 from text_classification.datasets import SSTDataset
 from text_classification.encoders import RNFEncoder
 from text_classification.models import RNF
+from text_classification.tokenizers import SpacyTokenizer
 from text_classification.vectors import GloVe
 from text_classification.vocab import Vocab
-from text_classification.tokenizers import SpacyTokenizer
-
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.callbacks.base import Callback
-
-import logging
 
 log = logging.getLogger(__name__)
 
-class LoggingCallback(Callback):
 
+class LoggingCallback(Callback):
     def on_epoch_end(self, trainer, pl_module):
         metrics = trainer.callback_metrics
         epoch = trainer.current_epoch
         log.info("Epoch: %s", epoch)
-        log.info("Training Acc: %.4f\t Training Loss: %.4f", metrics['train_epoch_acc'], metrics['train_epoch_loss'])
-        log.info("Validation Acc: %.4f\t Validation Loss: %.4f", metrics['val_epoch_acc'], metrics['val_epoch_loss'])
+        log.info(
+            "Training Acc: %.4f\t Training Loss: %.4f",
+            metrics["train_epoch_acc"],
+            metrics["train_epoch_loss"],
+        )
+        log.info(
+            "Validation Acc: %.4f\t Validation Loss: %.4f",
+            metrics["val_epoch_acc"],
+            metrics["val_epoch_loss"],
+        )
 
-early_stop_callback = EarlyStopping(
-   monitor='val_epoch_loss',
-   min_delta=0.0001,
-   patience=3,
-   verbose=False,
-   mode='min'
-)
-
-checkpoint_callback = ModelCheckpoint(
-    filepath='./checkpoints/'+'{epoch}',
-    save_top_k=1,
-    verbose=False,
-    monitor='val_epoch_loss',
-    mode='min'
-)
 
 @hydra.main(config_name="config")
 def main(cfg: DictConfig):
-    
+
     log.info("Arguments:\n %s", OmegaConf.to_yaml(cfg))
 
     seed_everything(42)
 
     if not cfg.dataset.fine_grained:
-        filter_func = lambda x: x.label != 'neutral'
+        filter_func = lambda x: x.label != "neutral"
     else:
         filter_func = None
 
     # hydra generates a new working directory for each run
     # want to store data in same directory each run
-    root=hydra.utils.to_absolute_path('.data')
-    
+    root = hydra.utils.to_absolute_path(".data")
+
     log.info("Downloading data...")
     # 1. Get SST dataset
-    train, val, test = SSTDataset(root=root, filter_func=filter_func,
-                        tokenizer=SpacyTokenizer(), **cfg.dataset)
+    train, val, test = SSTDataset(
+        root=root, filter_func=filter_func, tokenizer=SpacyTokenizer(), **cfg.dataset
+    )
 
     log.info("Creating vocab...")
     # 2. Get vocab
@@ -71,29 +65,55 @@ def main(cfg: DictConfig):
     # 3. Retrieve pre-trained embeddings
     vectors = GloVe(root=root, name=cfg.vectors.name, dim=300)
     embed_mat = vectors.get_matrix(vocab)
-    
+
     # 4. Setup encoder to encode examples
     encoder = RNFEncoder(vocab=vocab, target_encoding={"negative": 0, "positive": 1})
 
     # 5. Setup train, val and test dataloaders
-    ds = DataModule(train=train, val=val, test=test, encoder=encoder,
-                batch_size=cfg.datamodule.batch_size)
+    ds = DataModule(
+        train=train,
+        val=val,
+        test=test,
+        encoder=encoder,
+        batch_size=cfg.datamodule.batch_size,
+    )
 
     # 6. Setup model
     num_class = 5 if cfg.dataset.fine_grained else 2
-    model = RNF(input_size=len(vocab), num_class=num_class, embed_mat=embed_mat, **cfg.model)
+    model = RNF(
+        input_size=len(vocab), num_class=num_class, embed_mat=embed_mat, **cfg.model
+    )
 
     # 7. Setup trainer
-    trainer = Trainer(checkpoint_callback=checkpoint_callback,
-                     callbacks=[LoggingCallback(), early_stop_callback], 
-                     **cfg.trainer)
+    early_stop_callback = EarlyStopping(
+        monitor="val_epoch_loss",
+        min_delta=0.0001,
+        patience=3,
+        verbose=False,
+        mode="min",
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        filepath="./checkpoints/" + "{epoch}",
+        save_top_k=1,
+        verbose=False,
+        monitor="val_epoch_loss",
+        mode="min",
+    )
+
+    trainer = Trainer(
+        checkpoint_callback=checkpoint_callback,
+        callbacks=[LoggingCallback(), early_stop_callback],
+        **cfg.trainer
+    )
     log.info("Training...")
     # 8. Fit model
     trainer.fit(model, ds.train_dataloader(), ds.val_dataloader())
 
     # 9. Test model
-    results = trainer.test(model, ds.test_dataloader(), 
-            ckpt_path=checkpoint_callback.best_model_path)
+    results = trainer.test(
+        model, ds.test_dataloader(), ckpt_path=checkpoint_callback.best_model_path
+    )
 
     log.info(results)
 
