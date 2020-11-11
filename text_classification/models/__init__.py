20e205420f13c8fcddbecf1106cd4e63b27f0904
pbmstrk <pbmstrk@users.noreply.github.com>
2020-11-11 21:53:19 +0100

    Add transformer encoder model
    - add model
    - add tests

--- text_classification/models/__init__.py
+++ text_classification/models/__init__.py
@@ -1,3 +1,4 @@
 from .rnf import RNF
 from .textcnn import TextCNN
 from .nse import NSE
+from .transformer import Transformer
--- text_classification/models/textcnn.py
+++ text_classification/models/textcnn.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Tuple, Optional
 
 import numpy as np
 import torch
@@ -12,7 +12,7 @@ class TextCNN(nn.Module):
         input_size: int,
         num_class: int = 2,
         embed_dim: int = 300,
-        kernel_sizes: List[int] = [3, 4, 5],
+        kernel_sizes: Tuple[int] = (3, 4, 5),
         out_channels: int = 100,
         dropout: float = 0.5,
         embed_dropout: float = 0.4,
@@ -21,39 +21,29 @@ class TextCNN(nn.Module):
     ):
         super().__init__()
 
-        self.input_size = input_size
-        self.num_class = num_class
-        self.embed_dim = embed_dim
-        self.kernel_sizes = kernel_sizes
-        self.out_channels = out_channels
-        self.dropout = dropout
-        self.embed_dropout = embed_dropout
-        self.embed_mat = embed_mat
-        self.freeze_embed = freeze_embed
-
-        self.embedding = nn.Embedding(self.input_size, self.embed_dim, padding_idx=0)
-        if self.embed_mat is not None:
+        self.embedding = nn.Embedding(input_size, embed_dim, padding_idx=0)
+        if embed_mat is not None:
             self.embedding = self.embedding.from_pretrained(
-                torch.from_numpy(self.embed_mat).float()
+                torch.from_numpy(embed_mat).float()
             )
-        if self.freeze_embed:
+        if freeze_embed:
             self.embedding.weight.requires_grad = False
 
         self.convs = nn.ModuleList(
             [
                 nn.Conv1d(
-                    in_channels=self.embed_dim,
-                    out_channels=self.out_channels,
+                    in_channels=embed_dim,
+                    out_channels=out_channels,
                     kernel_size=fs,
                 )
-                for fs in self.kernel_sizes
+                for fs in kernel_sizes
             ]
         )
 
-        self.fc = nn.Linear(len(self.kernel_sizes) * self.out_channels, self.num_class)
+        self.fc = nn.Linear(len(kernel_sizes) * out_channels, num_class)
 
-        self.drop = nn.Dropout(self.dropout)
-        self.embed_drop = nn.Dropout(self.embed_dropout)
+        self.drop = nn.Dropout(dropout)
+        self.embed_drop = nn.Dropout(embed_dropout)
 
     def forward(self, batch):
 
