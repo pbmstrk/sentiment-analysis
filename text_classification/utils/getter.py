ad313d5165f279a0a662da2ef6a5ffdf2a63c20d
pbmstrk <pbmstrk@users.noreply.github.com>
2020-11-13 21:46:36 +0100

    Added no_decay list to get_optimizer

--- text_classification/utils/getter.py
+++ text_classification/utils/getter.py
@@ -18,9 +18,20 @@ def check_scheduler_args(func, name, args):
                 pname in args.keys()
             ), f"{name} expects a value for {pname}"
 
-def get_optimizer(model, name, args):
+def get_optimizer(model, name, args, no_decay=("bias", "LayerNorm.weight")):
     opt = try_get_function(torch.optim, name)
-    return opt(model.parameters(), **args)
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "weight_decay": args.get("weight_decay", 0.0),
+        },
+        {
+            "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+            "weight_decay": 0.0,
+        },
+    ]
+    args.pop("weight_decay", None)
+    return opt(optimizer_grouped_parameters, **args)
 
 def get_scheduler(optimizer, name, args):
 
