617200ce5e88f6d3b4a0c59514e36cd3e553f0ba
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2020-11-19 14:31:24 +0100

    Add requirements.txt file (#47)
    * Add requirements file and adapt setup.py
    
    * Format setup.py
    
    * Update requirements file
    
    Co-authored-by: pbmstrk <pbmstrk@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
+import os
+
 from setuptools import setup, find_packages
 
 
-def get_install_requires():
+PATH_ROOT = os.path.dirname(__file__)
 
-    return ["pytorch_lightning>=1.0.3", "spacy", "nltk", "torch>=1.3"]
+
+def load_requirements(
+    path_dir=PATH_ROOT, file_name="requirements.txt", comment_char="#"
+):
+    with open(os.path.join(path_dir, file_name), "r") as file:
+        lines = [ln.strip() for ln in file.readlines()]
+    reqs = []
+    for ln in lines:
+        # filer all comments
+        if comment_char in ln:
+            ln = ln[: ln.index(comment_char)].strip()
+        if ln:  # if requirement is not empty
+            reqs.append(ln)
+    return reqs
 
 
 def get_extras_require():
@@ -29,6 +44,6 @@ setup(
     name="text_classification",
     author="Paul Baumstark",
     packages=find_packages(exclude=("tests", "scripts")),
-    install_requires=get_install_requires(),
+    install_requires=load_requirements(),
     extras_require=get_extras_require(),
 )
