2cab7b778efddcca12109f7e98e6de4a2d0dcb04
pbmstrk <pbmstrk@users.noreply.github.com>
2020-11-19 14:27:13 +0100

    Format setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ from setuptools import setup, find_packages
 
 PATH_ROOT = os.path.dirname(__file__)
 
-def load_requirements(path_dir=PATH_ROOT, file_name='requirements.txt', comment_char='#'):
-    with open(os.path.join(path_dir, file_name), 'r') as file:
+
+def load_requirements(
+    path_dir=PATH_ROOT, file_name="requirements.txt", comment_char="#"
+):
+    with open(os.path.join(path_dir, file_name), "r") as file:
         lines = [ln.strip() for ln in file.readlines()]
     reqs = []
     for ln in lines:
         # filer all comments
         if comment_char in ln:
-            ln = ln[:ln.index(comment_char)].strip()
+            ln = ln[: ln.index(comment_char)].strip()
         if ln:  # if requirement is not empty
             reqs.append(ln)
     return reqs
